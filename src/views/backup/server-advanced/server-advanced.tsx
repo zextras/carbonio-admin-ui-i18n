c931c738c0d8361bc8324c2b483b0783967c951c
zextras_kapil <kapil.nadiyapara@zextras.com>
2022-09-26 15:17:30 +0530

    chore: set value on advanced configuration page

--- src/views/backup/server-advanced/server-advanced.tsx
+++ src/views/backup/server-advanced/server-advanced.tsx
@@ -3,7 +3,7 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import React, { FC } from 'react';
+import React, { FC, useContext, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
 import {
@@ -13,13 +13,144 @@ import {
 	Divider,
 	Button,
 	Switch,
-	Input
+	Input,
+	SnackbarManagerContext,
+	Padding
 } from '@zextras/carbonio-design-system';
+import {
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore
+	getSoapFetchRequest
+} from '@zextras/carbonio-shell-ui';
 import ListRow from '../../list/list-row';
+import { useServerStore } from '../../../store/server/store';
 
 const ServerAdvanced: FC = () => {
 	const { operation, server }: { operation: string; server: string } = useParams();
+	const allServers = useServerStore((state) => state.serverList);
 	const [t] = useTranslation();
+	const createSnackbar: any = useContext(SnackbarManagerContext);
+	const [isDirty, setIsDirty] = useState<boolean>(false);
+	const [ldapDumpEnabled, setLdapDumpEnabled] = useState<boolean>(false);
+	const [serverConfiguration, setServerConfiguration] = useState<boolean>(false);
+	const [purgeOldConfiguration, setPurgeOldConfiguration] = useState<boolean>(false);
+	const [includeIndex, setIncludeIndex] = useState<boolean>(false);
+	const [backupLatencyLowThreshold, setBackupLatencyLowThreshold] = useState<number>(0);
+	const [backupLatencyHighThreshold, setBackupLatencyHighThreshold] = useState<number>(0);
+	const [backupMaxWaitTime, setBackupMaxWaitTime] = useState<number>(0);
+	const [backupMaxMetaDataSize, setBackupMaxMetaDataSize] = useState<number>(0);
+	const [backupOnTheFlyMetadata, setBackupOnTheFlyMetadata] = useState<boolean>(false);
+	const [backupMaxOperationPerAccount, setBackupMaxOperationPerAccount] = useState<number>(0);
+	const [backupCompressionLevel, setBackupCompressionLevel] = useState<number>(0);
+	const [backupNumberThreadsForItems, setBackupNumberThreadsForItems] = useState<number>(0);
+	const [backupNumberThreadsForAccounts, setBackupNumberThreadsForAccounts] = useState<number>(0);
+
+	const [scheduledMetadataArchivingEnabled, setScheduledMetadataArchivingEnabled] =
+		useState<boolean>(false);
+
+	useEffect(() => {
+		if (allServers && allServers.length > 0) {
+			const selectedServer = allServers.find((serverItem: any) => serverItem?.name === server);
+			if (selectedServer && selectedServer?.id) {
+				getSoapFetchRequest(
+					`/service/extension/zextras_admin/core/getServer/${selectedServer?.id}?module=zxbackup`
+				)
+					.then((data: any) => {
+						if (data && data?.attributes) {
+							const attributes = data?.attributes;
+							if (attributes?.ldapDumpEnabled) {
+								const value = attributes?.ldapDumpEnabled?.value;
+								if (value) {
+									setLdapDumpEnabled(value);
+								}
+							}
+
+							if (attributes?.backupLatencyLowThreshold) {
+								const value = attributes?.backupLatencyLowThreshold?.value;
+								if (value) {
+									setBackupLatencyLowThreshold(value);
+								}
+							}
+
+							if (attributes?.backupLatencyHighThreshold) {
+								const value = attributes?.backupLatencyHighThreshold?.value;
+								if (value) {
+									setBackupLatencyHighThreshold(value);
+								}
+							}
+
+							if (attributes?.ZxBackup_MaxWaitingTime) {
+								const value = attributes?.ZxBackup_MaxWaitingTime?.value;
+								if (value) {
+									setBackupMaxWaitTime(value);
+								}
+							}
+
+							if (attributes?.ZxBackup_MaxMetadataSize) {
+								const value = attributes?.ZxBackup_MaxMetadataSize?.value;
+								if (value) {
+									setBackupMaxMetaDataSize(value);
+								}
+							}
+
+							if (attributes?.backupOnTheFlyMetadata) {
+								const value = attributes?.backupOnTheFlyMetadata?.value;
+								if (value) {
+									setBackupOnTheFlyMetadata(value);
+								}
+							}
+
+							if (attributes?.scheduledMetadataArchivingEnabled) {
+								const value = attributes?.scheduledMetadataArchivingEnabled?.value;
+								if (value) {
+									setScheduledMetadataArchivingEnabled(value);
+								}
+							}
+
+							if (attributes?.ZxBackup_MaxOperationPerAccount) {
+								const value = attributes?.ZxBackup_MaxOperationPerAccount?.value;
+								if (value) {
+									setBackupMaxOperationPerAccount(value);
+								}
+							}
+
+							if (attributes?.backupCompressionLevel) {
+								const value = attributes?.backupCompressionLevel?.value;
+								if (value) {
+									setBackupCompressionLevel(value);
+								}
+							}
+
+							if (attributes?.backupNumberThreadsForItems) {
+								const value = attributes?.backupNumberThreadsForItems?.value;
+								if (value) {
+									setBackupNumberThreadsForItems(value);
+								}
+							}
+
+							if (attributes?.backupNumberThreadsForAccounts) {
+								const value = attributes?.backupNumberThreadsForAccounts?.value;
+								if (value) {
+									setBackupNumberThreadsForAccounts(value);
+								}
+							}
+						}
+					})
+					.catch((error: any) => {
+						createSnackbar({
+							key: 'error',
+							type: 'error',
+							label: error?.message
+								? error?.message
+								: t('label.something_wrong_error_msg', 'Something went wrong. Please try again.'),
+							autoHideTimeout: 3000,
+							hideButton: true,
+							replace: true
+						});
+					});
+			}
+		}
+	}, [server, allServers, createSnackbar, t]);
 	return (
 		<Container mainAlignment="flex-start" background="gray6">
 			<Container
@@ -28,13 +159,34 @@ const ServerAdvanced: FC = () => {
 				crossAlignment="flex-start"
 				mainAlignment="flex-start"
 			>
-				<Row mainAlignment="flex-start" padding={{ all: 'large' }}>
-					<Text size="medium" weight="bold">
-						{t('backup.advanced', 'Advanced')}
-					</Text>
+				<Row takeAvwidth="fill" mainAlignment="flex-start" width="100%">
+					<Container orientation="vertical" mainAlignment="space-around" height="56px">
+						<Row orientation="horizontal" width="100%">
+							<Row
+								padding={{ all: 'large' }}
+								mainAlignment="flex-start"
+								width="50%"
+								crossAlignment="flex-start"
+							>
+								<Text size="medium" weight="bold" color="gray0">
+									{t('backup.advanced', 'Advanced')}
+								</Text>
+							</Row>
+							<Row
+								padding={{ all: 'large' }}
+								width="50%"
+								mainAlignment="flex-end"
+								crossAlignment="flex-end"
+							>
+								<Padding right="small">
+									{isDirty && <Button label={t('label.cancel', 'Cancel')} color="secondary" />}
+								</Padding>
+								{isDirty && <Button label={t('label.save', 'Save')} color="primary" />}
+							</Row>
+						</Row>
+					</Container>
+					<Divider color="gray2" />
 				</Row>
-
-				<Divider />
 
 				<Container
 					mainAlignment="flex-start"
@@ -49,18 +201,32 @@ const ServerAdvanced: FC = () => {
 							mainAlignment="flex-start"
 							crossAlignment="flex-start"
 						>
-							<Switch label={t('backup.ldap_dump', 'LDAP Dump')} />
+							<Switch
+								label={t('backup.ldap_dump', 'LDAP Dump')}
+								value={ldapDumpEnabled}
+								onClick={(): void => setLdapDumpEnabled(!ldapDumpEnabled)}
+							/>
 						</Container>
 						<Container padding={{ top: 'large' }}>
 							<Switch
 								label={t('backup.include_server_configuration', 'Include server configuration')}
+								value={serverConfiguration}
+								onClick={(): void => setServerConfiguration(!serverConfiguration)}
 							/>
 						</Container>
 						<Container padding={{ top: 'large' }}>
-							<Switch label={t('backup.purge_old_configuration', 'Purge old configuration')} />
+							<Switch
+								label={t('backup.purge_old_configuration', 'Purge old configuration')}
+								value={purgeOldConfiguration}
+								onClick={(): void => setPurgeOldConfiguration(!purgeOldConfiguration)}
+							/>
 						</Container>
 						<Container padding={{ top: 'large' }}>
-							<Switch label={t('backup.include_index', 'Include index')} />
+							<Switch
+								label={t('backup.include_index', 'Include index')}
+								value={includeIndex}
+								onClick={(): void => setIncludeIndex(!includeIndex)}
+							/>
 						</Container>
 					</ListRow>
 
@@ -115,8 +281,10 @@ const ServerAdvanced: FC = () => {
 							<Input
 								label={t('backup.latency_high_threshold_ms', 'Latency High Threshold (ms)')}
 								background="gray5"
-								backgroundColor="gray5"
-								borderColor="gray3"
+								value={backupLatencyHighThreshold}
+								onChange={(e: any): any => {
+									setBackupLatencyHighThreshold(e.target.value);
+								}}
 							/>
 						</Container>
 						<Container
@@ -129,8 +297,10 @@ const ServerAdvanced: FC = () => {
 							<Input
 								label={t('backup.latency_low_threshold_ms', 'Latency Low Threshold (ms)')}
 								background="gray5"
-								backgroundColor="gray5"
-								borderColor="gray3"
+								value={backupLatencyLowThreshold}
+								onChange={(e: any): any => {
+									setBackupLatencyLowThreshold(e.target.value);
+								}}
 							/>
 						</Container>
 					</ListRow>
@@ -157,8 +327,11 @@ const ServerAdvanced: FC = () => {
 							<Input
 								label={t('backup.max_waiting_time_ms', 'Max Waiting Time (ms)')}
 								background="gray5"
-								backgroundColor="gray5"
 								borderColor="gray3"
+								value={backupMaxWaitTime}
+								onChange={(e: any): any => {
+									setBackupMaxWaitTime(e.target.value);
+								}}
 							/>
 						</Container>
 					</ListRow>
@@ -185,8 +358,10 @@ const ServerAdvanced: FC = () => {
 							<Input
 								label={t('backup.maximum_metadata_size_mb', 'Maximum Metadata Size (MB)')}
 								background="gray5"
-								backgroundColor="gray5"
-								borderColor="gray3"
+								value={backupMaxMetaDataSize}
+								onChange={(e: any): any => {
+									setBackupMaxMetaDataSize(e.target.value);
+								}}
 							/>
 						</Container>
 					</ListRow>
@@ -199,7 +374,11 @@ const ServerAdvanced: FC = () => {
 							padding={{ top: 'large', right: 'large' }}
 							width="100%"
 						>
-							<Switch label={t('backup.on_the_fly_metadata', 'On the fly metadata')} />
+							<Switch
+								label={t('backup.on_the_fly_metadata', 'On the fly metadata')}
+								value={backupOnTheFlyMetadata}
+								onClick={(): void => setBackupOnTheFlyMetadata(!backupOnTheFlyMetadata)}
+							/>
 						</Container>
 					</ListRow>
 
@@ -211,7 +390,13 @@ const ServerAdvanced: FC = () => {
 							padding={{ top: 'large', right: 'large' }}
 							width="100%"
 						>
-							<Switch label={t('backup.metadata_archiving', 'Metadata archiving')} />
+							<Switch
+								label={t('backup.metadata_archiving', 'Metadata archiving')}
+								value={scheduledMetadataArchivingEnabled}
+								onClick={(): void =>
+									setScheduledMetadataArchivingEnabled(!scheduledMetadataArchivingEnabled)
+								}
+							/>
 						</Container>
 					</ListRow>
 
@@ -237,8 +422,10 @@ const ServerAdvanced: FC = () => {
 							<Input
 								label={t('backup.maximum_operation_per_account', 'Maximum Operation per Account')}
 								background="gray5"
-								backgroundColor="gray5"
-								borderColor="gray3"
+								value={backupMaxOperationPerAccount}
+								onChange={(e: any): any => {
+									setBackupMaxOperationPerAccount(e.target.value);
+								}}
 							/>
 						</Container>
 						<Container
@@ -251,8 +438,10 @@ const ServerAdvanced: FC = () => {
 							<Input
 								label={t('backup.compression_level', 'Compression Level')}
 								background="gray5"
-								backgroundColor="gray5"
-								borderColor="gray3"
+								value={backupCompressionLevel}
+								onChange={(e: any): any => {
+									setBackupCompressionLevel(e.target.value);
+								}}
 							/>
 						</Container>
 					</ListRow>
@@ -268,8 +457,10 @@ const ServerAdvanced: FC = () => {
 							<Input
 								label={t('backup.thread_number_for_items', 'Thread number for items')}
 								background="gray5"
-								backgroundColor="gray5"
-								borderColor="gray3"
+								value={backupNumberThreadsForItems}
+								onChange={(e: any): any => {
+									setBackupNumberThreadsForItems(e.target.value);
+								}}
 							/>
 						</Container>
 						<Container
@@ -282,8 +473,10 @@ const ServerAdvanced: FC = () => {
 							<Input
 								label={t('backup.thread_number_for_accounts', 'Thread number for accounts')}
 								background="gray5"
-								backgroundColor="gray5"
-								borderColor="gray3"
+								value={backupNumberThreadsForAccounts}
+								onChange={(e: any): any => {
+									setBackupNumberThreadsForAccounts(e.target.value);
+								}}
 							/>
 						</Container>
 					</ListRow>
