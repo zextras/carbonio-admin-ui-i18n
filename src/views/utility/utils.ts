fde5f2ec4048163d44d5a1968e5d8bfa045bc26e
zextras_kapil <kapil.nadiyapara@zextras.com>
2022-11-25 17:28:06 +0530

    fix: adding email address to mailing list not working

--- src/views/utility/utils.ts
+++ src/views/utility/utils.ts
@@ -1476,8 +1476,7 @@ export const getFormatedDate = (date: Date): any => {
 };
 
 export const isValidEmail = (email: string): boolean => {
-	const re = /^\w+([\\.-]?\w+)*@\w+([\\.-]?\w+)*(\.\w{2,3})+$/;
-	// const re = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+	const re = /^\w+([\\.-]?\w+)*@\w+([\\.-]?\w+)*(\.\w{2,20})+$/;
 	return re.test(email);
 };
 
