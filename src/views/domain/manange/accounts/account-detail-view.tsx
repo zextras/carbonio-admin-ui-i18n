211ba96d9e13398dad088f25054d3f93cb15273d
mananpatel13589 <38516903+mananpatel13589@users.noreply.github.com>
2022-12-01 01:00:54 +0530

    chore: account alias changes

--- src/views/domain/manange/accounts/account-detail-view.tsx
+++ src/views/domain/manange/accounts/account-detail-view.tsx
@@ -16,7 +16,9 @@ import {
 	Quota,
 	Button,
 	Modal,
-	SnackbarManagerContext
+	SnackbarManagerContext,
+	Chip,
+	Divider
 } from '@zextras/carbonio-design-system';
 import { Trans, useTranslation } from 'react-i18next';
 import styled from 'styled-components';
@@ -56,6 +58,7 @@ const AccountDetailView: FC<any> = ({
 	const [isOpenDeleteDialog, setIsOpenDeleteDialog] = useState<boolean>(false);
 	const [isRequestInProgress, setIsRequestInProgress] = useState<boolean>(false);
 	const createSnackbar: any = useContext(SnackbarManagerContext);
+	const [accountAliases, setAccountAliases] = useState<any[]>([]);
 
 	const getDataSourceDetail = useCallback((): void => {
 		getMailboxQuota(selectedAccount?.id).then((data) => {
@@ -65,6 +68,13 @@ const AccountDetailView: FC<any> = ({
 	useEffect(() => {
 		getDataSourceDetail();
 	}, [getDataSourceDetail]);
+
+	useEffect(() => {
+		if (accountDetail?.mail) {
+			const aliaes = accountDetail.mail.split(', ').map((ele: string) => ({ label: ele }));
+			setAccountAliases(aliaes);
+		}
+	}, [accountDetail?.mail]);
 
 	const onDeleteAccount = useCallback(() => {
 		setIsOpenDeleteDialog(true);
@@ -301,6 +311,32 @@ const AccountDetailView: FC<any> = ({
 								value={selectedAccount?.name || ''}
 								readOnly
 							/>
+						</Row>
+					</Row>
+				</Row>
+				<Row width="100%" padding={{ top: 'large' }}>
+					<Row width="100%" mainAlignment="flex-start" crossAlignment="flex-start">
+						<Row padding={{ left: 'large', bottom: 'small' }}>
+							<Text size="small" color="secondary">
+								{t('account_details.aliases', 'Aliases')}
+							</Text>
+						</Row>
+						<Row width="95%">
+							<Container
+								orientation="horizontal"
+								wrap="wrap"
+								mainAlignment="flex-start"
+								maxWidth="44rem"
+								style={{ gap: '0.5rem' }}
+								padding={{ left: 'large' }}
+							>
+								{accountAliases?.map(
+									(ele, index) => index > 0 && <Chip key={`chip${index}`} label={ele.label} />
+								)}
+							</Container>
+							<Row width="100%" padding={{ top: 'medium' }}>
+								<Divider color="gray2" />
+							</Row>
 						</Row>
 					</Row>
 				</Row>
