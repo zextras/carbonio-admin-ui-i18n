06ebaab565afc3c161075568949a5d6a5673a5b0
zextras_kapil <kapil.nadiyapara@zextras.com>
2023-03-07 15:34:41 +0530

    chore: update selection of configuration when select server

--- src/views/backup/configuration/backup-configuration.tsx
+++ src/views/backup/configuration/backup-configuration.tsx
@@ -440,12 +440,28 @@ const BackupConfiguration: FC = () => {
 			})
 			.catch((error: any) => {
 				setIsSaveRequestInProgress(false);
+				setCurrentBackupValue((prev: any) => ({
+					...prev,
+					moduleEnableStartup,
+					enableRealtimeScanner,
+					runSmartScanStartup,
+					spaceThreshold,
+					isScheduleSmartScan,
+					scheduleSmartScan,
+					scheduleAutomaticRetentionPolicy,
+					retentionPolicySchedule,
+					backupDestPath,
+					keepDeletedItemInBackup,
+					keepDeletedAccountsInBackup
+				}));
+				setIsDirty(false);
 				createSnackbar({
-					key: 'error',
-					type: 'error',
-					label: error
-						? error?.error
-						: t('label.something_wrong_error_msg', 'Something went wrong. Please try again.'),
+					key: 'success',
+					type: 'success',
+					label: t(
+						'label.the_last_changes_has_been_saved_successfully',
+						'Changes have been saved successfully'
+					),
 					autoHideTimeout: 3000,
 					hideButton: true,
 					replace: true
